f231d14e2fa3be69c846c7d2d44a1980d7264365
Dharsan <dharsan.s212@gmail.com>
2026-07-04 14:24:45 +0530

    Dharsan Navbar ajiopng update

--- frontend/frontend/src/Components/Navbar.jsx
+++ frontend/frontend/src/Components/Navbar.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import "../Styles/Navbar.css";
 
 export default function Navbar() {
+  let username=localStorage.getItem('name')
+  let navigate=useNavigate()
   const [menuOpen, setMenuOpen] = useState(false);
+  const handleLogout=()=>{
+     localStorage.removeItem("name");
+      navigate('/')
+  }
 
   return (
     <div className="navbar">
@@ -15,14 +21,21 @@ export default function Navbar() {
       >
         ☰
       </div>
+      
+      <div>
+    </div>
 
       <div className={`nav ${menuOpen ? "active" : ""}`}>
         <Link to="/home">Home</Link>
         <Link to="/">Compare</Link>
-
-        <button className="login-btn"><Link to='/'>Login</Link></button>
-        <button className="register-btn"><Link to='/register'>Register</Link></button>
+        <div className="welcome">
+    <h3>Welcome, {username}! 👋</h3>
+    <div className="logout">
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  </div>
       </div>
     </div>
   );
 }
+
