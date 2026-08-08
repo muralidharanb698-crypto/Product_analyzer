5600049dfa215724372c7fc0364b169100ef240d
muralidharanb698-crypto <muralidharanb698@gmail.com>
2026-08-08 21:18:56 +0530

    Fix Render build path

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+set -o errexit
+
 pip install -r requirements.txt
 
 playwright install chromium
 
+cd backend
+
 python manage.py migrate
 
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
